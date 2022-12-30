57fd683e803e577cf72d5607c81c9d00f46a3b7c
zephyr005 <rajkumar.rbj20@gmail.com>
2022-12-30 19:03:24 +0530

    Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,15 +2,16 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        CurrentAccount account1 = new CurrentAccount("Ram",10000.00,"ABCD");
+        CurrentAccount account1 = new CurrentAccount("Ram",10000.00,"ABCDDFF");
         account1.validateLicenseId();
+        System.out.println(account1.getTradeLicenseId());
 
         SavingsAccount account2 = new SavingsAccount("Sohan",5000.00,2000,15.6);
-        account2.withdraw(3000);
-        account2.withdraw(6000);
+        //account2.withdraw(3000);
+        //account2.withdraw(6000);
         account2.withdraw(1000);
-        account2.getSimpleInterest(2);
-        account2.getCompoundInterest(3,1);
+        System.out.println(account2.getSimpleInterest(2));
+        System.out.println(account2.getCompoundInterest(3,1));
 
         StudentAccount account3 = new StudentAccount("Raj Kumar",1000,"AccioJob");
     }
